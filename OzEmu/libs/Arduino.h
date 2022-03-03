15946b092b03a46f614672c03dd3ea4e77714d2a
papakarlo787 <evgeniy8a8p@gmail.com>
2022-03-03 15:35:08 +0500

    bugs around flashing fixed

--- OzEmu/libs/Arduino.h
+++ OzEmu/libs/Arduino.h
@@ -5,6 +5,8 @@
 #define HIGH 1
 #define LOW 0
 #define OZEMU
+#define PROGMEM
+
 
 
 uint32_t millis(){
@@ -15,7 +17,9 @@ uint32_t micros(){
 	return (uint32_t)clock();
 }
 
-unsigned int analogRead(unsigned int _){}
+unsigned int analogRead(uint8_t _){}
+unsigned int analogWrite(uint8_t _, uint16_t __){}
+unsigned int pgm_read_byte(uint16_t __){}
 
 unsigned int random(unsigned int max){
 	return rand() % max;
