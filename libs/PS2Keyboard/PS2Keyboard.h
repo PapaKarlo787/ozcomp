cbe664679e0a497f3167d5670ed7f6d309ec947f
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2022-03-11 19:28:48 +0500

    Update PS2Keyboard.h

--- libs/PS2Keyboard/PS2Keyboard.h
+++ libs/PS2Keyboard/PS2Keyboard.h
@@ -2,7 +2,7 @@ class PS2Keyboard {
   public:
     PS2Keyboard();
     static bool available();
-    static void begin(uint8_t dataPin);
+    static void begin();
     static uint8_t read();
   private:
 	
