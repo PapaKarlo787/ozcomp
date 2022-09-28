f4b1f2b495d41a3ee38dbe7a76a97eaffa6f807f
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2022-09-29 00:23:47 +0300

    no input delay

--- arch/inputs.h
+++ arch/inputs.h
@@ -1,7 +1,6 @@
 void get_key() {
 	keyboard.write(0);
 	setFlags(R[15] = keyboard.read());
-	delay(2);
 }
 
 void get_mice() {
@@ -10,17 +9,14 @@ void get_mice() {
 	for (uint8_t i = 0; i < 24; i+=8)
 		R[15] |= keyboard.read() << i;
 	setFlags(R[15]);
-	delay(2);
 }
 
 void get_joy() {
 	keyboard.write(2);
 	setFlags(R[15] = keyboard.read());
-	delay(2);
 }
 
 void get_IR() {
 	keyboard.write(3);
 	setFlags(R[15] = keyboard.read());
-	delay(2);
 }
